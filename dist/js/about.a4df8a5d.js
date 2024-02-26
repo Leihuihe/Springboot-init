2fe559f74b272871baa4eae5ec0190aa4bbea74b
Leihuihe <leihui.he@qq.com>
2024-02-25 21:43:20 -0600

    Add: add dist for deployment

---
+++ dist/js/about.a4df8a5d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkantd_search"]=self["webpackChunkantd_search"]||[]).push([[443],{4007:function(n,a,t){t.r(a),t.d(a,{default:function(){return d}});var e=t(3396);const r={class:"about"},s=(0,e._)("h1",null,"This is an about page",-1),u=[s];function c(n,a){return(0,e.wg)(),(0,e.iD)("div",r,u)}var i=t(89);const h={},o=(0,i.Z)(h,[["render",c]]);var d=o}}]);
+//# sourceMappingURL=about.a4df8a5d.js.map
